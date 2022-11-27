e18947c530b4d0f64ae52bbc398b0b3a5610d2da
Jamila <jamilali.dev@gmail.com>
2022-11-27 14:17:11 +0400

    Initial Commit

---
+++ test2222/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2222
+//
+//  Created by Jamila  on 27.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
